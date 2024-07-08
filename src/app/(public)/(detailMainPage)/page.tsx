7389790dcd3b5e8a59e23660fef4bc17302ab59e
minsu <alstn549@naver.com>
2024-07-08 20:27:03 +0900

    setting2

---
+++ src/app/(public)/(detailMainPage)/page.tsx
@@ -0,0 +1,14 @@
+import Comment from '@/components/Comment';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      영역
+      {/* ===================================================== */}
+      <Comment />
+    </div>
+  );
+};
+
+export default page;
