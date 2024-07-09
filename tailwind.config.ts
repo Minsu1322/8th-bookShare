b66008d5c6cf01680b0b71fde4f3b0c952e51d95
hj7321 <oboki2001@naver.com>
2024-07-09 12:43:19 +0900

    style: 커스텀 클래스 정의(main 색상)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        main: '#AF5858'
       }
     }
   },
