72c3e6138747173325aa976ffa79a043e2e3fd71
kimyong <right4570@naver.com>
2024-07-10 14:26:12 +0900

    제목 : ui 업데이트 update: mypage변경버튼 클릭 시 input으로 변경하는 로직으로 업데이트

--- src/app/(private)/(mypage)/mypage/page.tsx
+++ src/app/(private)/(mypage)/mypage/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 import Image from 'next/image';
-import React, { useState } from 'react';
-import UserInfo from './_components/userInfo';
-import CommentList from './_components/commentlist';
-import LikedList from './_components/likedlist';
-import Button from '@/components/Button';
+import React, { useRef, useState } from 'react';
+import CommentList from './_components/Commentlist';
+import LikedList from './_components/Likedlist';
+import UserInfo from './_components/UserInfo';
+import { toast } from 'react-toastify';
 
 const taps = [
   { label: '회원정보', content: <UserInfo /> },
@@ -14,6 +14,30 @@ const taps = [
 
 const Mypage = () => {
   const [activeTab, setActiveTab] = useState(0);
+  const avatarImgRef = useRef(null);
+
+  const handleAvatarUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files!;
+    const fileExtension = ['.jpg', '.jpeg', '.png', '.gif'];
+    if (!files['0']) {
+      toast.error('아바타 업로드 취소하셨습니다.', {
+        position: 'top-right'
+      });
+      return;
+    }
+    const userUploadfileExtension = files['0'].name.split('.').pop()?.toLowerCase();
+    if (!fileExtension.includes(`.${userUploadfileExtension}`)) {
+      console.error('지원하지 않는 파일 형식입니다. JPG, JPEG, PNG, GIF 파일만 업로드 가능합니다.');
+      toast.error('지원하지 않는 파일 형식입니다. JPG, JPEG, PNG, GIF 파일만 업로드 가능합니다.', {
+        position: 'top-right'
+      });
+      // 에러 처리 로직 추가
+      return;
+    }
+    console.log(files['0'].name.split('.').pop()?.toLowerCase());
+  };
+
+  //   const data = await getUserInfo();
   return (
     <>
       <div className="flex justify-between sm:w-[1280px] mx-auto items-center">
@@ -21,10 +45,22 @@ const Mypage = () => {
           <div className="w-[100px] h-[100px] rounded-full overflow-hidden mb-[10px]">
             <Image src="/images/noImg.png" width={100} height={100} alt="avatarImg" className="block w-full h-full " />
           </div>
-          <input 
-            className={'text-base text-[#af5858] font-bold text-center mt-[30px] w-[80px] h-[30px] mb-[30px] bg-white rounded-full'}
-            value={"업로드"}
-          />
+          <div>
+            <label
+              htmlFor="avatarImgUpload"
+              className="text-base text-[#af5858] font-bold text-center mt-[30px] w-[80px] h-[30px] mb-[30px] bg-white rounded-full cursor-pointer flex items-center justify-center"
+            >
+              업로드
+            </label>
+            <input
+              id="avatarImgUpload"
+              ref={avatarImgRef}
+              type="file"
+              className="hidden"
+              accept=".jpg,.jpeg,.png,.gif"
+              onChange={handleAvatarUpload}
+            />
+          </div>
           <h3 className="text-white text-xl font-bold">환영합니다.</h3>
           <p className="text-white text-xl font-bold mb-[30px]">르탄이님</p>
           <nav className="w-full">
