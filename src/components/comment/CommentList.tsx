bdd03b6983a94f3ea414cb794a93aa217c616922
jognhoon <jognhoon7431@gmail.com>
2024-07-13 18:59:33 +0900

    fix: 로그인 상태 확인 후, 로그인 페이지로 리다이렉트 안 되던 부분 수정(prefetch=>push) chore: 주석 제거 fix: 댓글 작성 직후에 key가 없다는 경고(작성할 때 id값도 직접 추가.uuidv4)

--- src/components/comment/CommentList.tsx
+++ src/components/comment/CommentList.tsx
@@ -30,8 +30,6 @@ const CommentList = ({ isEdit, setIsEdit, setTargetValue, user }: Props) => {
   const offset: number = (page - 1) * pageSize;
   const commentsToDisplay = comments?.slice(offset, offset + pageSize);
 
-  // const [totalPages] = useState(() => Math.ceil(comments ? comments.length / 6: ));
-
   const [editingId, setEditingId] = useState<string | null>(null);
 
   const handleEdit = (comment: Tables<'comments'>) => {
@@ -87,16 +85,6 @@ const CommentList = ({ isEdit, setIsEdit, setTargetValue, user }: Props) => {
     }
   });
 
-  // const deleteMutation = useMutation({
-  //   mutationFn: deleteComment,
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries({ queryKey: ['comments', postId] });
-  //     toast.success('삭제 완료');
-  //   },
-  //   onError: (error) => {
-  //     toast.error(error.message);
-  //   }
-  // });
   const handleDelete = (id: string) => {
     if (!confirm('삭제시 복구가 어렵습니다. 정말 삭제하시겠습니까?')) {
       return;
